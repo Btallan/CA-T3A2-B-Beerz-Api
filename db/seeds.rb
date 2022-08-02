e9a6e4b02602ac4d0a5e986c07114d458aa351c9
Lachlan Brasell <92556836+Lachlan-Br@users.noreply.github.com>
2022-08-02 17:45:31 +1000

    Schema-comments-seeds updated
    Placed test comments into seeds - updated db and migrated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if message.count == 0
+    message.create(text: "example text 1")
+    message.create(text: "example text 2")
+    message.create(text: "example text 3")
+    
