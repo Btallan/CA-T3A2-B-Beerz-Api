a2f7d6e6f3fd8fb0267d00bef3469f56ea70fa15
Lachlan Brasell <92556836+Lachlan-Br@users.noreply.github.com>
2022-08-04 23:16:31 +1000

    secure password update
    App/models/user.rb - updated secure password so no passwords are stored in database - example test added too

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    #has: first_name:string - last_name:string - email:string - username:string - date_of_birth:date - mobile:string - employer:boolean - admin:boolean
+    #has: password_digestLstring but is used as secure.
+    #secure has added in : password:string (virtual) and password_confirmation:string (virtual)
+    #User exmaple test information:
+    #User.create({first_name: "test", last_name: "test_last", email: "email@testing.com", username: "testuser", mobile: "mobiletest", employer: true, admin: true, password: "password", password_confirmation: "password"} 
 end
